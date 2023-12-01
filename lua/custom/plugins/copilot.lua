a565c384f22fd7020bc1e79f4715767018b15670
Austin Wallace ubpc <austinwallace196@gmail.com>
2023-12-01 17:33:41 -0500

    neogit

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,11 +1,13 @@
 return {
   "github/copilot.vim",
   config = function ()
-    
-    vim.keymap.set("i", "<M-CR>", 'copilot#Accept("<CR>")', { expr = true })
-    vim.keymap.set("i", "<C-f>", 'copilot#Accept("<CR>")', { expr = true })
-    vim.keymap.set('i', '<M-k>', '<Plug>(copilot-next)')
-    vim.keymap.set('i', '<M-j>', '<Plug>(copilot-previous)')
+    local map = vim.keymap.set
+    map("i", "<C-f>", "copilot#Accept('<CR>')", {noremap = true, silent = true, expr=true, replace_keycodes = false })
+    map("i", "<M-CR>", 'copilot#Accept("<CR>")', {noremap = true, silent = true, expr=true, replace_keycodes = false })
+    map('i', '<M-k>', '<Plug>(copilot-next)')
+    map('i', '<M-j>', '<Plug>(copilot-previous)')
+    map('i', '<M-l>', '<Plug>(copilot-select)')
+
     vim.g.copilot_filetypes = {
         tex = false,
     }
