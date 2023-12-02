43e17f551422a3bec60461b40de4f779acf28d4f
Austin Wallace ubpc <austinwallace196@gmail.com>
2023-12-01 22:30:13 -0500

    vimtex

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -1,3 +1,16 @@
 return {
   "lervag/vimtex",
+  config = function()
+    vim.g.vimtex_view_method = 'zathura'
+    vim.g.vimtex_view_general_viewer = 'okular'
+    vim.cmd [[
+  let g:vimtex_quickfix_open_on_warning = 0
+  ]]
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = "*.tex",
+      command = "silent VimtexCompileSS",
+      desc = "Compiles Latex"
+    })
+    vim.g.vimtex_compiler_method = 'latexmk'
+  end
 }
