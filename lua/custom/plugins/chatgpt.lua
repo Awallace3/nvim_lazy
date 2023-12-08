defbd55788f98841dc2d87b50743f5ea0fe54b4d
Austin Wallace ubpc <austinwallace196@gmail.com>
2023-12-07 19:30:14 -0500

    chatgpt updates

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -129,7 +129,7 @@ return {
         model = "gpt-4",
         frequency_penalty = 0,
         presence_penalty = 0,
-        max_tokens = 4000,
+        max_tokens = 12000,
         temperature = 0,
         top_p = 1,
         n = 1,
