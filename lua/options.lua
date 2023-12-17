330651c9d39aa07d25a3d51d8921f0819e648435
Austin Wallace <austinwallace196@gmail.com>
2023-12-17 00:31:36 -0500

    clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -18,7 +18,8 @@ vim.o.mouse = 'a'
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+-- vim.o.clipboard = 'unnamedplus'
+-- vim.o.clipboard = ''
 
 -- Enable break indent
 vim.o.breakindent = true
