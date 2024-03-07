34275d8d9460417c230abf5258107d21c125afeb
Austin Wallace <awallace43@gatech.edu>
2024-03-07 09:58:22 -0500

    neotest added

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -3,43 +3,43 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter"
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-python"
   },
+  lazy = true,
   init = function()
     local nt = require("neotest")
-
     nt.setup({
-        adapters = {
-            require("neotest-python")({
-                dap = {justMyCode = true},
-                args = {"--log-level", "DEBUG"},
-                runner = "pytest",
-                python = "${CONDA_PREFIX/bin/python}",
-            })
-        },
-        discovery = {
-          concurrent = 1,
-          enabled = true,
-        },
-        floating = {
-          border = "rounded",
-          max_height = 0.9,
-          max_width = 0.8,
-          options = {}
-        },
-        icons={
-          passed = "",
-          running = "",
-          failed = "",
-          unknown = "",
-        },
-        quickfix = {open=false},
-        output = {open_on_run=false},
-        lazy=true,
+      adapters = {
+        require("neotest-python")({
+          -- dap = { justMyCode = true, },
+          args = { "--log-level", "DEBUG" },
+          runner = "pytest",
+        })
+      },
+      discovery = {
+        concurrent = 1,
+        enabled = true,
+      },
+      floating = {
+        border = "rounded",
+        max_height = 0.9,
+        max_width = 0.8,
+        options = {}
+      },
+      icons = {
+        passed = "",
+        running = "",
+        failed = "",
+        unknown = "",
+      },
+      quickfix = { open = false },
+      output = { open_on_run = false },
+      lazy = true,
     })
     nt.state.adapter_ids()
     local keymap = vim.api.nvim_set_keymap
-    local opts = {noremap = true}
+    local opts = { noremap = true }
     keymap('n', '[j', '<CMD>lua require("neotest").jump.next({status = "failed"})<CR>', opts)
     keymap('n', ']j', '<CMD>lua require("neotest").jump.prev({status = "failed"})<CR>', opts)
   end,
