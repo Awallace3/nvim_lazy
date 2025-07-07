a8b2660d47b0b88173e5d5dcca64e18cb4ec4967
AMW Arch <awallace43@gatech.edu>
2025-07-07 14:04:41 -0400

    indent

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -22,37 +22,42 @@
 --     },
 --   },
 return {
-  'saghen/blink.nvim',
-  keys = {
-	-- chartoggle
-	{
-	  ';',
-	  function()
-	  	require('blink.chartoggle').toggle_char_eol(';')
-	  end,
-	  mode = { 'n', 'v' },
-	  desc = 'Toggle ; at eol',
-	},
-	{
-	  ',',
-	  function()
-	  	require('blink.chartoggle').toggle_char_eol(',')
-	  end,
-	  mode = { 'n', 'v' },
-	  desc = 'Toggle , at eol',
-	},
+	'saghen/blink.nvim',
+	keys = {
+		-- chartoggle
+		{
+			';',
+			function()
+				require('blink.chartoggle').toggle_char_eol(';')
+			end,
+			mode = { 'n', 'v' },
+			desc = 'Toggle ; at eol',
+		},
+		{
+			',',
+			function()
+				require('blink.chartoggle').toggle_char_eol(',')
+			end,
+			mode = { 'n', 'v' },
+			desc = 'Toggle , at eol',
+		},
 
-	-- tree
-	{ '<C-e>', '<cmd>BlinkTree reveal<cr>', desc = 'Reveal current file in tree' },
-	{ '<leader>E', '<cmd>BlinkTree toggle<cr>', desc = 'Reveal current file in tree' },
-	-- { '<leader>e', '<cmd>BlinkTree toggle-focus<cr>', desc = 'Toggle file tree focus' },
-  },
-  -- all modules handle lazy loading internally
-  lazy = false,
-  opts = {
-    chartoggle = { enabled = true },
-    delimiters = { enabled = true },
-    indent = { enabled = true },
-    tree = { enabled = true }
-  }
+		-- tree
+		{ '<C-e>',     '<cmd>BlinkTree reveal<cr>', desc = 'Reveal current file in tree' },
+		{ '<leader>E', '<cmd>BlinkTree toggle<cr>', desc = 'Reveal current file in tree' },
+		-- { '<leader>e', '<cmd>BlinkTree toggle-focus<cr>', desc = 'Toggle file tree focus' },
+	},
+	-- all modules handle lazy loading internally
+	lazy = false,
+	opts = {
+		chartoggle = { enabled = true },
+		delimiters = { enabled = true },
+		indent = {
+			enabled = true,
+			blocked = {
+				filetypes = { 'dbout' }
+			}
+		},
+		tree = { enabled = true }
+	}
 }
